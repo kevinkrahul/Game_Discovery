58ae16c5cdf2d2e8811beb63458f1cd70dff8779
kevin <kevinrahul.22ec@kct.ac.in>
2025-01-22 20:03:53 +0530

    Responsive layout created

--- game-discovery/src/App.tsx
+++ game-discovery/src/App.tsx
@@ -1,13 +1,30 @@
-import { Button } from "@chakra-ui/react"
-
-
-
+import { Button, Show } from "@chakra-ui/react";
+import { Grid, GridItem,useBreakpointValue } from "@chakra-ui/react";
 const App = () => {
+
+  const showAside=useBreakpointValue({base:false,sm:true});
+
   return (
     <div>
-      <Button colorPalette="teal" size="xl" variant="solid" >Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          sm: `"nav nav" "aside main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="red">
+          Nav
+        </GridItem>
+        { showAside && <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>}
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- game-discovery/src/components/ui/provider.tsx
+++ game-discovery/src/components/ui/provider.tsx
@@ -1,21 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 import { defaultSystem } from "@chakra-ui/react";
 import React from "react";
 
-interface props{
-    children:React.ReactNode;
+interface props {
+  children: React.ReactNode;
 }
-
 //export function Provider(props:{children:React.ReactNode}){}
-
-
-export function Provider({children}:props){
-    return(
-        <ChakraProvider value={defaultSystem}>
-            {/* <ThemeProvider> */}
-                {children}
-            {/* </ThemeProvider>     */}
-        </ChakraProvider>
-    )
+export function Provider({ children }: props) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      {/* <ThemeProvider> */}
+      {children}
+      {/* </ThemeProvider>     */}
+    </ChakraProvider>
+  );
 }
