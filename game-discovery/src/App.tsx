fac5c2fac43e5a16c7fcf4508dd36c02e422dfc8
kevin <kevinrahul.22ec@kct.ac.in>
2025-01-30 18:36:22 +0530

    Filtered by platforms

--- game-discovery/src/App.tsx
+++ game-discovery/src/App.tsx
@@ -6,12 +6,13 @@ import GenresList from "./components/GenresList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 const App = () => {
   const showAside = useBreakpointValue({ base: false, sm: true });
 
   const [selectedGenre,setSelectedGenre]=useState<Genre | null>(null)
-
+  const [selectedPlatform,setSelectedPlatform]=useState<Platform | null>(null)
   return (
     <>
       <Grid
@@ -36,8 +37,8 @@ const App = () => {
           </GridItem>
         )}
         <GridItem area="main">
-          <PlatformSelector />
-          <GameGrid selectedGenre={selectedGenre}/>
+          <PlatformSelector selectedPlatform={selectedPlatform} onSelectedPlatform={(platform)=>setSelectedPlatform(platform)}/>
+          <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
         </GridItem>
       </Grid>
     </>
--- game-discovery/src/components/PlatformSelector.tsx
+++ game-discovery/src/components/PlatformSelector.tsx
@@ -1,9 +1,15 @@
-import { Game } from '@/hooks/useGames'
-import { Button, Menu ,MenuButton,MenuList,MenuItem} from '@chakra-ui/react'
+import { Button, Menu ,MenuButton,MenuList,MenuItem, Text} from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 import useplatform from '../hooks/usePlatforms'
+import { Platform } from '@/hooks/useGames'
 
-const PlatformSelector = () => {
+interface Props{
+  onSelectedPlatform:(platform:Platform)=>void;
+  selectedPlatform:Platform | null;
+}
+
+
+const PlatformSelector = ({onSelectedPlatform,selectedPlatform}:Props) => {
     const {data,error}=useplatform();
 
     if (error) return null;
@@ -11,10 +17,11 @@ const PlatformSelector = () => {
   return (
     <Menu>
         <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-            Platforms
+            {/* {selectedPlatform ? <Text>{selectedPlatform.name}</Text>:<Text>Platform</Text> } */}
+            {selectedPlatform?.name || 'Platform'}
         </MenuButton>
         <MenuList>
-            {data.map(platform=> <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
+            {data.map(platform=> <MenuItem onClick={()=>onSelectedPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
         </MenuList>
     </Menu>
   )
