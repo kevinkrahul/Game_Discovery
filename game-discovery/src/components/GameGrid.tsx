00217be1595bef30d84c3d188812966062ce5088
kevin <kevinrahul.22ec@kct.ac.in>
2025-02-07 16:13:39 +0530

    Refactor game grid

--- game-discovery/src/components/GameGrid.tsx
+++ game-discovery/src/components/GameGrid.tsx
@@ -5,38 +5,36 @@ import GameCarSkeleton from "./GameCarSkeleton";
 import GameCardContainer from "./GameCardContainer";
 // import { Genre } from "@/hooks/useGenres";
 import { GameQuery } from "@/App";
-interface Props{
-  gamequery:GameQuery;
+interface Props {
+  gamequery: GameQuery;
   // selectedGenre:Genre | null;
   // selectedPlatform:Platform | null;
 }
 
 // const GameGrid = ({selectedGenre,selectedPlatform}:Props) => {
-const GameGrid = ({gamequery}:Props) => {
+const GameGrid = ({ gamequery }: Props) => {
   const { data, error, isLoading } = useGames(gamequery); // selectedGenre,selectedPlatform as gamequery
   const skeleton = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeleton.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCarSkeleton  children={'200px'}/>
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard  game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeleton.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCarSkeleton children={"200px"} />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
